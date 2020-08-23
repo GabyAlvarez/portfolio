32d5110a282f7161d1cf9913ede4c860f8ef68c7
Gaby Alvarez <gabyalvarzb@gamil.com>
2020-08-23 01:10:38 -0500

    adding proyect burger

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -6,7 +6,22 @@ const react = "../images/react.png"
 
 const projects = [
     {
-        "id":"0",
+        "id": "0",
+        "name": "Burger Station",
+        "img": "../images/burger1.png",
+        "text": "Estoy desarrollando una single page application (SPA), donde se gestiona el pedido, preparación y entrega de las ordenes, teniendo una comunicación fluida entre mesero y cocinero.",
+        "tools": [
+            html,
+            css,
+            js,
+            firebase,
+            react
+        ],
+        "linkGithub":"https://github.com/GabyAlvarez/CDMX009-BurgerQueen"
+
+    },
+    {
+        "id":"1",
         "name":"TripLife",
         "img": "../images/main-TripLife.png",
         "text": "Red social para viajeros la cual te permite ingresar mediante Facebook o Google, editar tu información de perfil, compartir historias, fotos y tips y reaccionar a los distintos post.",
@@ -20,7 +35,7 @@ const projects = [
 
     },
     {
-        "id": "1",
+        "id": "3",
         "name": "Pokedex",
         "img": "../images/pokedex.png",
         "text": "Participé en la construcción de una página web dirigida aquellas personas que gustan de Pokémon para consultar las características de sus personajes favoritos.",
@@ -29,22 +44,7 @@ const projects = [
             css,
             js
         ],
-        "linkGithub":"https://github.com/GabyAlvarez/CDMX009-Social-Network"
-    },
-    {
-        "id": "2",
-        "name": "Definir",
-        "img": "../images/cardPortfolio.jpg",
-        "text": "Participe en la construcción de una página web dirigida aquellas personas que gustan de Pokémon para consultar las características de sus personajes favoritos.",
-        "tools": [
-            html,
-            css,
-            js,
-            firebase,
-            react
-        ],
-        "linkGithub":"https://github.com/GabyAlvarez/CDMX009-Social-Network"
-
+        "linkGithub":"https://github.com/GabyAlvarez/CDMX009-Data-Lovers"
     }
 ]
 
